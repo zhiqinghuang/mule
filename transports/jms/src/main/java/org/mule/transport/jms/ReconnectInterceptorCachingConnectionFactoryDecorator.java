8ea348c9b30bfd8f90c15be44c9f974c86d9a5fd
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-01-11 18:18:56 -0300

    Intercepting caching connection factory for reconnnection interception

---
+++ transports/jms/src/main/java/org/mule/transport/jms/ReconnectInterceptorCachingConnectionFactoryDecorator.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.transport.jms;
+
+import org.mule.api.MuleContext;
+
+import javax.jms.ConnectionFactory;
+
+import org.springframework.jms.connection.CachingConnectionFactory;
+
+public class ReconnectInterceptorCachingConnectionFactoryDecorator implements ConnectionFactoryDecorator
+{
+
+    private ReconnectInterceptorCachingConnectionFactory reconnectInterceptorCachingConnectionFactory;
+
+    @Override
+    public ConnectionFactory decorate(ConnectionFactory connectionFactory, JmsConnector jmsConnector, MuleContext mulecontext)
+    {
+        reconnectInterceptorCachingConnectionFactory = new ReconnectInterceptorCachingConnectionFactory(connectionFactory);
+
+        return reconnectInterceptorCachingConnectionFactory;
+    }
+
+    @Override
+    public boolean appliesTo(ConnectionFactory connectionFactory, MuleContext muleContext)
+    {
+        return connectionFactory instanceof CachingConnectionFactory;
+    }
+}
